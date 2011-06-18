8c527872b887937cdfae8d876a3af10a869f8191
jlew <jlew.blackout@gmail.com>
2011-06-18 17:49:10 -0400

    Menus now put old symposiums in a sub menu

--- models/menu.py
+++ models/menu.py
@@ -15,19 +15,39 @@ response.meta.generator = 'Web2py Enterprise Framework'
 response.meta.copyright = 'Copyright 2011'
 
 #Symposium list required for Symposium and paper Menus
-symposiums = db(db.symposium.id > 0).select(orderby=~db.symposium.event_date)
+symposiums_past = db(db.symposium.event_date < request.now.date()).select(orderby=~db.symposium.event_date)
+symposiums = db(db.symposium.event_date >= request.now.date()).select(orderby=~db.symposium.event_date)
 
 # Build Symposium Menu
-symp_list =  [(db.symposium._format % x, False,
+current_symp = [(db.symposium._format % x, False,
                 URL('papers', 'index', args=x.sid), [
                     (T('View Participants'), False, URL('people', 'index', args=x.sid), []),
                     (T('View Agenda'), False, URL('agenda', 'index', args=x.sid), []),
                     (T('View Papers'), False, URL('papers', 'index', args=x.sid), []),
-                ]) for x in symposiums]
+                ]
+            ) for x in symposiums]
+
+past_symp = [(db.symposium._format % x, False,
+                URL('papers', 'index', args=x.sid), [
+                    (T('View Participants'), False, URL('people', 'index', args=x.sid), []),
+                    (T('View Agenda'), False, URL('agenda', 'index', args=x.sid), []),
+                    (T('View Papers'), False, URL('papers', 'index', args=x.sid), []),
+                ]
+            ) for x in symposiums_past]
+
+if past_symp:
+    current_symp.append( (T("Past Symposiums"), False, "#", past_symp) )
+
 
 # Build Paper Menu
 paper_symp_list = [(db.symposium._format % x, False,
                 URL('papers', 'index', args=x.sid), []) for x in symposiums]
+
+paper_symp_list_past = [(db.symposium._format % x, False,
+                URL('papers', 'index', args=x.sid), []) for x in symposiums_past]
+
+if paper_symp_list_past:
+    paper_symp_list.append( (T("Past Symposiums"), False, "#", paper_symp_list_past) )
 
 # We will show the manage and submit to all users so they know how to access
 # the paper management system when by logging in
@@ -43,7 +63,7 @@ if auth.has_membership("Reviewer"):
 
 response.menu = [
     (T('Home'), False, URL('default','index'), []),
-    (T('Symposiums'), False, "#", symp_list),
+    (T('Symposiums'), False, "#", current_symp),
     (T('Papers'), False, "#", paper_menu),
     ]
     
