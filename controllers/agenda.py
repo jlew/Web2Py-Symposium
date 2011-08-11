cce7e1e192ff0b7ad5908058b5eafb8de7d3df77
jlew <jlew.blackout@gmail.com>
2011-08-10 20:22:07 -0400

    Datatable agenda

--- controllers/agenda.py
+++ controllers/agenda.py
@@ -3,12 +3,15 @@
 def index():
     symp = db(db.symposium.sid==request.args(0)).select().first()
     if symp:
-        response.files.append(URL('static','week-cal/libs/css/smoothness/jquery-ui-1.8.11.custom.css'))
-        response.files.append(URL('static','week-cal/jquery.weekcalendar.css'))
-        response.files.append(URL('static','week-cal/skins/default.css'))
-        response.files.append(URL('static','week-cal/skins/gcalendar.css'))
-        response.files.append(URL('static','week-cal/jquery.weekcalendar.js'))
-        return dict(symposium=symp, papers=get_symposium_visable_papers(symp))
+        #response.files.append(URL('static','week-cal/libs/css/smoothness/jquery-ui-1.8.11.custom.css'))
+        #response.files.append(URL('static','week-cal/jquery.weekcalendar.css'))
+        #response.files.append(URL('static','week-cal/skins/default.css'))
+        #response.files.append(URL('static','week-cal/skins/gcalendar.css'))
+        #response.files.append(URL('static','week-cal/jquery.weekcalendar.js'))
+        papers = get_symposium_visable_papers(symp)
+        import datetime
+        papers.sort(key=lambda x: x.schedule_start if (x.schedule_start and x.scheduled) else datetime.time())
+        return dict(symposium=symp, papers=papers)
     else:
         raise HTTP(404)
 
