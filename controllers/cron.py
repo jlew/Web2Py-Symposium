ef026b666967a6c24e4e7521f18ad9ac487ede49
jlew <jlew.blackout@gmail.com>
2011-11-26 10:56:56 -0500

    Crontab to use controller (so path is not absolute)

---
+++ controllers/cron.py
@@ -0,0 +1,24 @@
+# coding: utf8
+# try something like
+def index(): return dict(message="hello from cron.py")
+
+def email_incomplete_papers():
+    if request.env.remote_addr not in ["127.0.0.1"]:
+        raise HTTP(401, 'unauthorized')
+
+    status_filter = False
+    for status_option in [PAPER_STATUS[x] for x in NEED_SUBMIT]:
+        if status_filter:
+            status_filter = status_filter | (db.paper.status == status_option)
+        else:
+            status_filter = (db.paper.status == status_option)
+    
+    papers = db(status_filter).select()
+    for paper in papers:
+        email_list = []
+        for author in paper.authors:
+            email_list.append(db.auth_user[author].email)
+        message = response.render('email_templates/paper_waiting_for_submission.txt', {'paper':paper})
+        mail.send(to=email_list,
+            subject=T("Symposium Paper Submission, Action Required"),
+            message=message)
